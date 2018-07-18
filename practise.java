afdfc6e6a39213aeddd505670a867cb980a172d9
hope4 <naveen.kumar131196@gmail.com>
2018-07-18 17:30:09 +0530

    First commit

---
+++ practise.java
@@ -0,0 +1,14 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class practise{
+
+    public static void main(String[] args){
+   GUIMakerClass myGUI = new GUIMakerClass();
+   myGUI.creatGUI();
+
+    }
+
+}
